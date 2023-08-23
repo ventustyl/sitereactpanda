d5ccdecc9c455ac5043ea6986d544b9d04e32f4f
Eric Venturino <eric.venturino@orange.fr>
2023-08-23 08:38:11 +0200

    package modif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PageIndex from './components/PageIndex';
-import PageProduits from './components/PageProduits'
-import PagePacks from './components/PagePacks';
-import PagePropos from './components/PagePropos';
-import PageForum from './components/PageForum';
-import PageContact from './components/PageContact';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageIndex from "./components/PageIndex";
+import PageProduits from "./components/PageProduits";
+import PagePacks from "./components/PagePacks";
+import PagePropos from "./components/PagePropos";
+import PageForum from "./components/PageForum";
+import PageContact from "./components/PageContact";
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path ="/" element={<PageIndex />} />
-      <Route path='/PageProduits' element= {<PageProduits/>}/>
-      <Route path='/PagePacks' element= {<PagePacks/>}/>
-      <Route path='/PagePropos' element= {<PagePropos/>}/>
-      <Route path='/PageForum' element= {<PageForum/>}/>
-      <Route path='/PageContact' element= {<PageContact/>}/>
-    </Routes>
-
+      <Routes>
+        <Route path="/sitereactpanda/" element={<PageIndex />} />
+        <Route path="/sitereactpanda/PageProduits" element={<PageProduits />} />
+        <Route path="/sitereactpanda/PagePacks" element={<PagePacks />} />
+        <Route path="/sitereactpanda/PagePropos" element={<PagePropos />} />
+        <Route path="/sitereactpanda/PageForum" element={<PageForum />} />
+        <Route path="/sitereactpanda/PageContact" element={<PageContact />} />
+      </Routes>
     </Router>
   );
 }
